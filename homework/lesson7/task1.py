eb52373c9f00c3a6977d2855f87595152036abe2
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2022-09-27 18:57:18 +0200

    Update task1.py

--- homework/lesson7/task1.py
+++ homework/lesson7/task1.py
@@ -1,1 +1,14 @@
+'''Task 1
 
+A simple function.
+
+Create a simple function called favorite_movie, which takes a string containing the name of your favorite movie.
+e function should then print "My favorite movie is named {name}".'''
+
+
+def favourite_movie():
+    film = ("Godfather")
+    print(f"My favourite movie is", film)
+
+print(favourite_movie())
+  
