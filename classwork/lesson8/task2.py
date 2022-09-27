f1bee718779e1757a30347965dc5f6a2d4797aea
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2022-09-27 19:49:56 +0200

    Create task2.py

---
+++ classwork/lesson8/task2.py
@@ -0,0 +1,7 @@
+'''You have a list [1, 2, 3, 4, 5, 6, 7, 0]. Write a function 
+"try_to_return_a_number", which will take an argument "number" and raise an exception OddError if number is odd, and ZeroError
+ if number is zero. Write a high level function,  which will take an argument "list_of_numbers", 
+ go through it in for in loop, put every item from list to "try_to_return_a_number" function, and
+ print a number or a message depends on exception on each loop step'''
+
+
