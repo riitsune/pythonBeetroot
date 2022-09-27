499fb5c49909d2d93a574c58b943237b6ecac331
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2022-09-27 18:57:57 +0200

    Create task2.py

---
+++ homework/lesson7/task2.py
@@ -0,0 +1,10 @@
+'''Task 2
+
+Creating a dictionary.
+
+Create a function called make_country, which takes in a country’s name and capital as parameters. 
+Then create a dictionary from those parameters, with ‘name’ and ‘capital’ as keys.
+Make the function print out the values of the dictionary to make sure that it works as intended.'''
+
+
+
