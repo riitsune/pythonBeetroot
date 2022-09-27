4de44c048819bf4da2492328fdf62e64852f44b6
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2022-09-27 19:50:24 +0200

    Update task1.py

--- classwork/lesson8/task1.py
+++ classwork/lesson8/task1.py
@@ -10,4 +10,19 @@ try:
 	1/new_int
 except print(ValueError, e):
 	print("Wrong input, please type number")
+	
+	
+#second way	
+user_input = input("Enter your value: ")
+
+def some_function (argument):
+    try:
+        user_input_int = int(user_input)
+        return 1/user_input_int
+    except ValueError:
+        print("Your input is incorrect, use an int")
+    except ZeroDivisionError:
+        print("You cannot divide on zero, try another number")
+
+print(some_function(user_input))
   
