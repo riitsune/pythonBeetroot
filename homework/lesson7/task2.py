2dd13163d65f6ebc1f35b5157ae1d65af475e46d
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2022-10-01 16:51:34 +0200

    Update task2.py

--- homework/lesson7/task2.py
+++ homework/lesson7/task2.py
@@ -7,4 +7,13 @@ Then create a dictionary from those parameters, with ‘name’ and ‘capital�
 Make the function print out the values of the dictionary to make sure that it works as intended.'''
 
 
+def make_country(country, capital):
+    country_dict ={'Country':country, "Capital": capital}
+    return country_dict
 
+
+country_name = input("Type here name of a country\n")
+capital_name = input("Type here country's capital\n")
+
+
+print(make_country(country_name, capital_name))
