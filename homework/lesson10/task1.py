ff47a2c10dfa53e33816445f82525d65bc9fef07
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2022-10-05 13:56:29 +0200

    Create task1.py

---
+++ homework/lesson10/task1.py
@@ -0,0 +1,9 @@
+'''Task 1
+
+A Person class
+
+Make a class called Person. Make the __init__() method take firstname, lastname, and age as parameters and add them as attributes. 
+Make another method called talk() which makes prints a greeting from the person containing, for example like this:
+
+"Hello, my name is Carl Johnson and I’m 26 years old"
+'''
