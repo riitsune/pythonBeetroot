802b8d3ce7ef50a1636f54e7dcfd35dae5025961
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2022-10-29 20:02:20 +0200

    Update task2.py

--- homework2/lesson12/task2.py
+++ homework2/lesson12/task2.py
@@ -6,3 +6,17 @@ Doggy age
 Create a class Dog with class attribute age_factor equals to 7. Make __init__() which takes values for a dog’s age. 
 Then create a method human_age which returns the dog’s age in human equivalent.
 '''
+class Dog():
+    age_factor: int = 7
+
+    def __init__(self,name, age):
+        self.name = name
+        self.age = age
+
+    def human_age(self,age_factor, age):
+        return int(age)*age_factor
+
+
+dog_1 = Dog("Antonio", "5")
+
+print(dog_1.human_age(7,5))
