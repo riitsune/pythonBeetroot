ea8ca53835cbb59969306b416a512521a6223597
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2022-10-29 19:46:16 +0200

    Create task2.py

---
+++ homework2/lesson12/task2.py
@@ -0,0 +1,8 @@
+'''
+Task 2
+
+Doggy age
+
+Create a class Dog with class attribute age_factor equals to 7. Make __init__() which takes values for a dog’s age. 
+Then create a method human_age which returns the dog’s age in human equivalent.
+'''
