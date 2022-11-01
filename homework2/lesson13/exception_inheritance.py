99af3e8f82087f9e3d2b38bbcf8984ed22c22161
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2022-11-01 17:03:48 +0100

    Create exception_inheritance.py

---
+++ homework2/lesson13/exception_inheritance.py
@@ -0,0 +1,5 @@
+'''
+Create your own exception classes inherited from built in Python ones. 
+Inherit one of them directly from Exception class and second - from some more specific class like RecursionError or UnicodeDecodeError.
+'''
+
