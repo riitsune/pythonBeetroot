f5e773aa52093eb7acb25c1de0ae605e339b7099
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2022-11-12 13:18:40 +0100

    updated task

--- homework2/lesson13/exception_inheritance.py
+++ homework2/lesson13/exception_inheritance.py
@@ -3,3 +3,10 @@ Create your own exception classes inherited from built in Python ones.
 Inherit one of them directly from Exception class and second - from some more specific class like RecursionError or UnicodeDecodeError.
 '''
 
+class SpecialTypeException(Exception):
+    pass
+
+
+user_i = int(input("Please give me a number"))
+if not user_i.isdigit:
+    raise SpecialTypeException("Must be only numbers")
